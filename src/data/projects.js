116e389f3c41e71a4b65f978cf19e20dc83fc193
fetraandri <102484907+fetraandri@users.noreply.github.com>
2025-03-27 02:27:16 -0400

    feat: add CV and new portfolio project with associated assets

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,18 +1,20 @@
 import gmaoImage from '../assets/images/gmaosary.jpg'; 
 import signature from '../assets/images/signature.png';
 import logopassioncampagne from '../assets/images/passioncampagne.png';
+import yvonportfolioimg from '../assets/images/yvnportfolio.png';
+
 
 export const projects = [
   {
     id: 1,
-    title: "GMAO",
+    title: "GMAO APP",
     description: "GMAO est une application de gestion de maintenance assistée par ordinateur, développée avec React pour l'interface utilisateur, Supabase et PostgreSQL pour la gestion des données, et déployée sur Vercel pour une accessibilité optimale.",
     link: "https://gmao-app.vercel.app/",
     image: gmaoImage, 
   },
   {
     id: 2,
-    title: "Portfolio",
+    title: "Portfolio Tahiry",
     description: "Portfolio professionnelle  développée avec ReactJS, déployée sur Vercel ,avec des données statiques tout simplement",
     link: "https://bro-portfolio.vercel.app/",
     image: signature,
@@ -24,4 +26,12 @@ export const projects = [
     link: "https://www.passion-campagne.com/",
     image: logopassioncampagne,
   },
+  {
+    id: 4,
+    title: "Portfolio Yvon ",
+    description: "Portfolio professionnelle  développée avec ReactJS, déployée sur Vercel ,avec des données statiques tout simplement",
+    link: "https://yvonraharinjatovo.vercel.app/",
+    image: yvonportfolioimg,
+  },
+
 ];
